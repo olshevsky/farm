e98aba0c9d71ec92099c199dac56fda24eb95f0c
dev <artur.olshevsky@gmail.com>
2023-04-24 03:07:26 +0300

    cleanup

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -3,29 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAnimalRequest;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use App\Models\Animal;
-use Illuminate\Support\Facades\Validator;
 
 class AnimalController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function index()
     {
         return Inertia::render('Animals/Index', [
-            'animals' => Animal::all()
+            'animals' => Auth::user()->animals()->get()
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function create()
     {
@@ -35,14 +34,14 @@ class AnimalController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreAnimalRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(StoreAnimalRequest $request)
     {
         $animal = new Animal();
         $animal->fill($request->all());
-        $animal->user_id = auth()->user()->id;
+        $animal->user_id = Auth::user()->id;
         $animal->save();
 
         return redirect()->route('animals.index');
@@ -62,8 +61,8 @@ class AnimalController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Animal $animal
+     * @return \Inertia\Response
      */
     public function edit(Animal $animal)
     {
@@ -75,9 +74,9 @@ class AnimalController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param StoreAnimalRequest $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(StoreAnimalRequest $request, $id)
     {
@@ -88,8 +87,8 @@ class AnimalController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
